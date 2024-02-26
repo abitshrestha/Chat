658721b62122c13cb6c4bcf3220bd64f110dfa36
abitshrestha <141226473+abitshrestha@users.noreply.github.com>
2024-02-26 09:26:18 +0545

    Delete server directory

--- server/dist/routes/authRoute.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-import { loginController, signupController } from '../controllers/authController.js';
-export const authRouter = express.Router();
-authRouter.post('/signup', signupController);
-authRouter.post('/login', loginController);
--- server/src/routes/roomRoute.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createRoom, getRooms, updateRoom } from '../controllers/roomController.js';
-
-export const roomRouter=express.Router();
-
-roomRouter.post('/rooms',createRoom);
-roomRouter.get('/rooms',getRooms);
-roomRouter.put('/rooms/:id',updateRoom);
